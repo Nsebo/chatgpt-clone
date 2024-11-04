839d8f661d2ec258e74b8af51eefa6e3c0717348
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2024-11-04 14:05:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './routes/homepage/Homepage';
+import DashboardPage from './routes/dashboardPage/dashboardPage';
+import ChatPage from './routes/chatpage/chatpage';
+import RootLayout from './layouts/rootLayout/RootLayout';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import SignInPage from './routes/signInPage/signInPage';
+import SignUpPage from './routes/signUpPage/signUpPage';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/sign-in/*',
+        element: <SignInPage />,
+      },
+      {
+        path: '/sign-up/*',
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: '/dashboard',
+            element: <DashboardPage />,
+          },
+          {
+            path: '/dashboard/chats/:id',
+            element: <ChatPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
